b0766f8f151cc8a9b3ce3044932b7202aee92a3f
robajillo <rjillo@yahoo.com>
2020-09-26 16:48:43 +0300

    imported migrate and migrate command from flask migrate

--- manage.py
+++ manage.py
@@ -2,8 +2,15 @@ from app import create_app,db
 from app.models import User
 from flask_script import Manager,Server
 from app import create_app,db
+from  flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
 
 
+manager =  Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+manager.add_command('run',Server(use_debugger=True))
 
 @manager.shell
 def make_shell_context():
