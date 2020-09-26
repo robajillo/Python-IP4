7f42ff4b04b72ae7c29ff27d133b074649b264a4
robajillo <rjillo@yahoo.com>
2020-09-26 12:17:55 +0300

    created class DevConfig

--- config.py
+++ config.py
@@ -2,6 +2,13 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roba:access@localhost/blog'
 
-class Prodconfig(config):
+class ProdConfig(Config):
+
+    pass
+
+class DevConfig(Config):
+
+    DEBUG = True
     
+
 
