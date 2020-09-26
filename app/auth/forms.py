07214a31975784aa0cd621fb404f8d129e2f7297
robajillo <rjillo@yahoo.com>
2020-09-26 11:25:05 +0300

    imported validation errors from wtf forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,4 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+from wtforms import ValidationError
