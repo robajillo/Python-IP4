e6008ed2804d3086cacd8c8d488daa214961618b
robajillo <rjillo@yahoo.com>
2020-09-26 11:42:10 +0300

    imported loginform from forms

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,url_for,flash,request
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user
 
