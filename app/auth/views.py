1b8549918b1312bfd4894c940d90488a7b19b5a9
robajillo <rjillo@yahoo.com>
2020-09-26 11:41:04 +0300

    imported flask_login from login_user

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,7 @@ from flask import render_template,redirect,url_for,flash,request
 from ..models import User
 from .forms import RegistrationForm
 from .. import db
+from flask_login import login_user
 
 
 @auth.route('/register',methods = ["GET","POST"])
