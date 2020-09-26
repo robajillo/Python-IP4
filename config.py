3d08fc980cc25f8d267c24aa269e678c9659b42f
robajillo <rjillo@yahoo.com>
2020-09-26 17:16:48 +0300

    added secret key

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roba:access@localhost/blog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    
 class ProdConfig(Config):
 
     pass
