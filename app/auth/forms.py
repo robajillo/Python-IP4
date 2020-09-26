79060af63222912a1d49b0a35d3f3d8f15589dd5
robajillo <rjillo@yahoo.com>
2020-09-26 11:24:12 +0300

    imported user from models

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,4 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
+from ..models import User
