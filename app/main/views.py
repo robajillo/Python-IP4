e3961841671383cb748cee61da8fb03cbee28a1d
robajillo <rjillo@yahoo.com>
2020-09-27 12:27:24 +0300

    imported new form class UpdateProfile

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask_login import login_required,current_user
 from flask import render_template,request,redirect,url_for,abort
 from ..models import  User
+from .forms import UpdateProfile
 
 
 @main.route('/user/<uname>')
