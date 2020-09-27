03d9205757036fce7f8ca8088e2433436a901bf5
robajillo <rjillo@yahoo.com>
2020-09-27 12:22:08 +0300

    craeted route for profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,13 @@
 from flask_login import login_required,current_user
+from flask import render_template,request,redirect,url_for,abort
+from ..models import  User
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
