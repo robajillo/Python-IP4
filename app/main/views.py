fa4eb6bc338778c91171785768c4758ab540b6c7
robajillo <rjillo@yahoo.com>
2020-09-27 12:42:48 +0300

    imported photos

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask_login import login_required,current_user
 from flask import render_template,request,redirect,url_for,abort
 from ..models import  User
 from .forms import UpdateProfile
-from .. import db
+from .. import db,photos
 
 
 @main.route('/user/<uname>')
