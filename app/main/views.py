6479c8bd244555dd8e5495cbe68074a4b68e683c
robajillo <rjillo@yahoo.com>
2020-09-27 12:28:10 +0300

    imported database from our app

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask_login import login_required,current_user
 from flask import render_template,request,redirect,url_for,abort
 from ..models import  User
 from .forms import UpdateProfile
+from .. import db
 
 
 @main.route('/user/<uname>')
