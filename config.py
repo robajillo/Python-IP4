c5f284496ef96965c9690a9e4fe9ecd62d674e3d
robajillo <rjillo@yahoo.com>
2020-09-27 11:13:17 +0300

    created base url for qoutees

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roba:access@localhost/blog'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     
 class ProdConfig(Config):
 
