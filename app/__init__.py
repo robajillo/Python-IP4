3bb40058a8a0c4c24c61a5aaf4a4b8b487187650
robajillo <rjillo@yahoo.com>
2020-09-27 11:00:28 +0300

    created configurations

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
 
 
     # Initializing flask extensions
