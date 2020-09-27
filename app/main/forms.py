702338383287480c01a4475d75f6241f688958b2
robajillo <rjillo@yahoo.com>
2020-09-27 13:20:16 +0300

    created  BioForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,7 @@
+class BioForm(FlaskForm):
+    bio = TextAreaField('Write A Short Bio About You...')
+    submit = SubmitField('Submit')
+
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
