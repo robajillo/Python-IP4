08fb1c4f006579293eee2bb22e4f9f495aa3d14a
robajillo <rjillo@yahoo.com>
2020-09-27 10:34:13 +0300

    modified file

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from app.models import User
 from flask_script import Manager,Server
-from app import create_app,db
+
 from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
@@ -10,7 +10,7 @@ app = create_app('development')
 manager =  Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
-manager.add_command('run',Server(use_debugger=True))
+manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
