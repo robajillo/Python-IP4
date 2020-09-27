d765532b4ddb52cf8770e2495182fb4edf252e06
robajillo <rjillo@yahoo.com>
2020-09-27 12:56:22 +0300

    imported datetime

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -31,3 +33,4 @@ class User(db.Model):
     
     def __repr__(self):
         return f'User {self.username}'
+
